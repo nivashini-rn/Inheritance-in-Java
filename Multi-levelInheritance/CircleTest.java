524e76b82ede98fd6f83c3fa159b6e2a15219cd9
nivashini-rn <nivashirn.gmail.com>
2020-07-23 19:55:02 +0530

    MLI added

---
+++ Multi-levelInheritance/CircleTest.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Circle
+{
+double a,r;
+void input()
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter r : ");
+r = sc.nextDouble();
+}
+void process()
+{
+a = Math.PI*r*r;
+}
+void output()
+{
+System.out.println(a);
+}
+}
+class CircleTest
+{
+public static void main(String arg[])
+{ 
+Circle c = new Circle();
+c.input();
+c.process();
+c.output();
+}
+}
